776c26f0986d54dcc3331479f5a4ebdd82e12029
Aleksandr Varaksin <avaraksin@ozon.ru>
2022-11-11 17:48:55 +0300

    EF2

--- Lesson4/Program.cs
+++ Lesson4/Program.cs
@@ -4,8 +4,20 @@ using Microsoft.EntityFrameworkCore;
 
 var connectionString = "Data Source=91.219.6.251\\SQLEXPRESS; Initial Catalog=SimpleLogst; User Id=silog; Password=1234";
 
-var conn = new SqlConnection(connectionString);
-conn.Open();
+using (var conn = new SqlConnection(connectionString))
+{
+    conn.Open();
+
+    var SQLstring = @"IF OBJECT_ID('Clients') IS NULL
+                      BEGIN
+                          CREATE TABLE Clients (id INT NOT NULL, firstName VARCHAR(1000)); 
+                          ALTER TABLE Clients ADD CONSTRAINT PK_Clients PRIMARY KEY (id); 
+                      END;";
+    
+    var command = new SqlCommand(SQLstring, conn);
+    command.Connection.Open();
+    command.ExecuteNonQuery();
+}
 
 using (AppDBContext context = new AppDBContext(connectionString))
 {
