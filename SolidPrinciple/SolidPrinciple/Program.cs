d7b60e8a0c3b5d5049a72088449f35cb168eb85d
Alexander Varaksin <varaksin-ag@outlook.com>
2022-11-30 10:29:31 +0300

    SolidPrinciples. Получение настроек.

---
+++ SolidPrinciple/SolidPrinciple/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+using SolidPrinciple;
+
+    var builder = new ConfigurationBuilder()
+                   .SetBasePath(Directory.GetCurrentDirectory())
+                   .AddJsonFile("appsettings.json", optional: false);
+
+    IConfiguration config = builder.Build();
+
+    GameSettings ranges = new()
+    {
+        MinRange = int.Parse(config.GetSection("Ranges").GetSection("MinRange").Value),
+        MaxRange = int.Parse(config.GetSection("Ranges").GetSection("MaxRange").Value),
+        TryNumber = int.Parse(config.GetSection("TryNumber").Value)
+    };
+
+
+
+
+
