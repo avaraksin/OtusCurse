841e75cfe36ff00a76379fde3ca9ce2571b81025
Aleksandr Varaksin <avaraksin@ozon.ru>
2022-12-28 15:53:03 +0300

    Реализация GetMax

---
+++ Delegate/ExtentionClass.cs
@@ -0,0 +1,11 @@
+﻿namespace Delegate
+{
+    public static class ExtentionClass
+    {
+        public static T? GetMax<T>(this IEnumerable<T> e, Func<T, float> getParameter) 
+        {
+            return e.OrderByDescending(x => getParameter(x)).FirstOrDefault();
+        }
+
+    }
+}
---
+++ Delegate/Program.cs
@@ -0,0 +1,7 @@
+﻿using Delegate;
+
+Func<int, float> GetNumber = (n) => (float) n;
+
+IEnumerable<int> myList = new List<int>{ 1, 3, 6, 8, 10, 5 };
+
+Console.WriteLine(myList.GetMax(GetNumber));
