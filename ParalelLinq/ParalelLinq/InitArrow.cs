098df1c5f4923a33c9051dea87c542c8864a6e88
Alexander Varaksin <varaksin-ag@outlook.com>
2023-02-12 21:05:22 +0300

    InitProject

---
+++ ParalelLinq/ParalelLinq/InitArrow.cs
@@ -0,0 +1,21 @@
+﻿namespace ParalelLinq
+{
+    /// <summary>
+    /// Создаем начальный массив интов
+    /// </summary>
+    public class InitArrow
+    {
+        private const int _array = 10000000;
+       public List<long> initList { get; private set; }
+
+        public InitArrow() 
+        {
+            initList= new List<long>();
+            for(int i = 0; i < _array; i++)
+            {
+                var item = new Random().Next(0, 100000000);
+                initList.Add(item);
+            }
+        }
+    }
+}
---
+++ ParalelLinq/ParalelLinq/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using ParalelLinq;
+using System.Diagnostics;
+
+var initArraow = new InitArrow();
+var ints = initArraow.initList;
+
+Console.WriteLine($"Число элементов в массиве: {ints.Count()}");
+Console.WriteLine();
+
+var calc = new CalculateSum();
+
+Stopwatch Stopwatch= Stopwatch.StartNew();
+long sum = calc.CalculateByProcedure(ints);
+Stopwatch.Stop();
+Console.WriteLine($"Метод. Сумма: {sum}, Время (млс): {Stopwatch.Elapsed.TotalMilliseconds}");
